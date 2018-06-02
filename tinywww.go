85d9d09eae15e43256b11287a47e7d85a6fbd0e6
Diego Casati <diego.casati@gmail.com>
2018-06-01 23:09:52 -0700

    modify the Fprintf message - 2

--- tinywww.go
+++ tinywww.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    myname, err := os.Hostname()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Fprintf(w, "Hello BCIT from %s!", myname)
+	myname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, "Hi BCIT from %s!", myname)
 }
 
 func main() {
-    myport := ":8000"
-    http.HandleFunc("/", handler)
-    fmt.Println("Listening on port", myport)
-    http.ListenAndServe(myport, nil)
+	myport := ":8000"
+	http.HandleFunc("/", handler)
+	fmt.Println("Listening on port", myport)
+	http.ListenAndServe(myport, nil)
 }
